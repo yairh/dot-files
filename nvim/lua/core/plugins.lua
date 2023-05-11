baf42fb210b9d4ee806d7f514d9c895a7da40a67
Yair Hochner <yairhochner@gmail.com>
2023-05-11 22:17:17 +0300

    add lsp and fugitive

--- nvim/lua/core/plugins.lua
+++ nvim/lua/core/plugins.lua
@@ -38,6 +38,33 @@ return require('packer').startup(function(use)
   'nvim-lualine/lualine.nvim',
   requires = { 'nvim-tree/nvim-web-devicons', opt = true }
 }
+  use {'tpope/vim-fugitive'}
+
+  
+  use {
+  'VonHeikemen/lsp-zero.nvim',
+  branch = 'v2.x',
+  requires = {
+    -- LSP Support
+    {'neovim/nvim-lspconfig'},             -- Required
+    {                                      -- Optional
+      'williamboman/mason.nvim',
+      run = function()
+        pcall(vim.cmd, 'MasonUpdate')
+      end,
+    },
+    {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+    -- Autocompletion
+    {'hrsh7th/nvim-cmp'},     -- Required
+    {'hrsh7th/cmp-nvim-lsp'}, -- Required
+    {'L3MON4D3/LuaSnip'},     -- Required
+  }
+}
+  use { 'hrsh7th/cmp-path' }
+  use { 'hrsh7th/cmp-nvim-lsp' }
+  use { 'hrsh7th/cmp-buffer' }
+  use { 'saadparwaiz1/cmp_luasnip' }
   if packer_bootstrap then
     require('packer').sync()
   end
